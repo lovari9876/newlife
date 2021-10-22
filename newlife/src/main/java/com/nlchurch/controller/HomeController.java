63c8cf5da13215c83bf0c68332e43d5ba1e2d8da
lovari <lovari9876@gmail.com>
2021-10-22 16:40:23 +0900

    설정 마저 하고 폴더 새로 만들기..

--- newlife/src/main/java/com/nlchurch/controller/HomeController.java
+++ newlife/src/main/java/com/nlchurch/controller/HomeController.java
@@ -1,39 +1,65 @@
 package com.nlchurch.controller;
 
-import java.text.DateFormat;
-import java.util.Date;
+import java.io.File;
+import java.io.IOException;
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
-/**
- * Handles requests for the application home page.
- */
 @Controller
 public class HomeController {
-	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
+
+	/*
+	 * @Autowired private BoardService boardService;
+	 * 
+	 * @Autowired private AdminService adminService;
+	 * 
+	 * @Autowired private ContentService contentService;
+	 * 
+	 * @Autowired private MyPageService myPageService;
 	 */
+
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+	// 메인화면
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
+	public String home(Locale locale, Model model, Principal principal) throws Exception {
+
+		/*
+		 * // 히트다 히트 model.addAttribute("hit", boardService.selectHitList()); // 베스트
+		 * model.addAttribute("best", boardService.selectBestList()); // 랭킹(글 많이 쓴)
+		 * model.addAttribute("rankW", boardService.rankWrite()); // 랭킹(댓글 많이 쓴)
+		 * model.addAttribute("rankWC", boardService.rankWriteCo()); // 랭킹(신고많이받은)
+		 * model.addAttribute("rankRe", boardService.rankReport());
+		 */
+
+		/*
+		 * // 후원하기 위한 후원자 정보 if (principal != null) { String m_id = principal.getName();
+		 * MemberVO memberVO = myPageService.mypage(m_id);
+		 * model.addAttribute("memberVO", memberVO); }
+		 */
+
+		return "index";
 	}
-	
+
+
+
+
 }
